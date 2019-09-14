f514cc6bce16e5f97dc62e58f55ef08acdb47e4c
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-14 16:39:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+def line(array)
+  if array.length == 0 then 
+    puts "The line is currently empty."
+  else
+    line_string = "The line is currently:"
+    array.each_with_index { |person, index| line_string += " #{index + 1}. #{person}"}
+    puts line_string
+  end
+end
+
+def take_a_number(array, "Toby")
+  array.push("Toby")
+end
+  
+  
+  
+  
+  
+
+  
